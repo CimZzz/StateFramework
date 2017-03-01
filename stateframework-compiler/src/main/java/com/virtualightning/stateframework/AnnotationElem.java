f0479bddce464e5bca7831af72a5cb497a31cd77
xjw04 <1152564696@qq.com>
2017-03-01 16:33:15 +0800

    Build Commit 2017-3-1

---
+++ stateframework-compiler/src/main/java/com/virtualightning/stateframework/AnnotationElem.java
@@ -0,0 +1,23 @@
+package com.virtualightning.stateframework;
+
+import java.util.List;
+
+import javax.lang.model.type.TypeMirror;
+
+/**
+ * Created by CimZzz on 17/3/1.<br>
+ * Project Name : Virtual-Lightning StateFrameWork<br>
+ * Since : StateFrameWork_0.0.1<br>
+ * Description:<br>
+ * Description
+ */
+public class AnnotationElem {
+    String methodName;
+    String stateId;
+    boolean allowStop;
+    int runType;
+    int refType;
+
+    boolean isVarParameters;
+    List<TypeMirror> paramTypes;
+}
